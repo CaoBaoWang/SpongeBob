a84bb2a518bb5066430f96bfef4a69949e4fcc9b
wangshaoli <1125550225@qq.com>
2021-04-21 18:07:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+用css 画画海绵宝宝
+
+dev
+```
+yarn add parcel  
+
+parcel src/index.html
+
+```
+
+
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './reset.css'
+import './style.css'
+
+
+if (/(iPhone|iPad|iPod|iOS|(Android))/i.test(navigator.userAgent)) { //移动端
+    if(spongeBob)spongeBob.classList.add('mobile-scale')
+}
+
+
